ba94d421f6a08b04480b654e71fe59f2ab9d8d8d
LuisMartinez211 <136252859+LuisMartinez211@users.noreply.github.com>
2024-01-29 00:51:55 -0400

    actualizando App.jsx
    Agregándole código responsive en tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,23 @@ import Logo from './Assets/Logo.png';
 const App = () => {
   return (
     <div>
-      <div className="flex items-center -mb-12 px-10 py-5">
-        <img src={Logo} alt="Logo" className="-mt-12 w-40" />
-        <h1 className="text-5xl -mt-8 -pl-12 font-bold">Inventario</h1>
+      <div className="flex flex-col md:flex-row items-center justify-between -mb-12 px-5 md:px-10 py-5">
+        <img src={Logo} alt="Logo" className="-mt-12 md:mt-0 w-40 h-auto" />
+        <h1 className="text-3xl md:text-5xl font-bold text-center pb-8 md:text-left">Inventario</h1>
       </div>
-      <div id="juan" className="container mx-auto p-7 px-12">
+      <div id="juan" className="container mx-auto p-5 md:p-7 px-10 md:px-12">
         {/*pan superior */}
-        <div className="bg-[#B0867D] h-12 w-full mb-1 rounded-t-full"></div>
-        
+        <div className="bg-[#B0867D] h-6 md:h-12 w-full mb-1 md:mb-2 rounded-t-full"></div>
         
         <AddProductForm />
         <ProductList />
 
-  
         {/*pan inferior */}
-        <div className="bg-[#633B32] h-10 w-full -mt-5 rounded-b-md"></div>
-        <div className="bg-[#B0867D] h-10 w-full mt-1 rounded-b-md"></div>
+        <div className="bg-[#633B32] h-6 md:h-10 w-full -mt-5 md:-mt-2 rounded-b-md"></div>
+        <div className="bg-[#B0867D] h-6 md:h-10 w-full mt-1 rounded-b-md"></div>
       </div>
     </div>
   );
 };
 
 export default App;
-
